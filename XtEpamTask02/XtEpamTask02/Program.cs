8f7a7827c6f8cdaef9614ad33704627e42e6aaf6
Roman Vaskovtsov <roman.vas47@gmail.com>
2019-11-24 14:21:55 +0300

    A little start for a big brain work with classes task 02

---
+++ XtEpamTask02/XtEpamTask02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtEpamTask02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //claassssss wooooorkable place
+
+        }
+    }
+}
---
+++ XtEpamTask02/XtEpamTask02/Round.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtEpamTask02
+{
+    class Round
+    {
+        private float x;
+        public float X
+        {
+            get 
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        private float y;
+        public float Y
+        {
+            get 
+            {
+                return y; 
+            }
+            set 
+            {
+                y = value;
+            }
+        }
+        private float raduis;
+        public float Raduis
+        {
+            get 
+            {
+                return raduis;
+            }
+            set 
+            {
+                if (value > 0)
+                {
+                    raduis = value;
+                }
+                else 
+                {
+                    throw new Exception("Radius can't be negative");
+                }
+                
+            }
+        }
+
+
+
+    }
+}
