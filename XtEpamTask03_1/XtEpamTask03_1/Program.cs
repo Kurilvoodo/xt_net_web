bbfd3c4a35c12c2712d558d5394a054c9dfbb7ea
Roman Vaskovtsov <roman.vas47@gmail.com>
2019-12-09 17:03:34 +0300

    All changes for code till tasks 4 will be uplaoad in a couple of days.

---
+++ XtEpamTask03_1/XtEpamTask03_1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtEpamTask03_1 //LOST 
+{
+    class Program
+    {
+        #region CollectionMethod
+        /// <summary>
+        /// Remove every second human from the circle
+        /// </summary>
+        /// <param name="people"></param>
+        /// <param name=""></param>
+        /// 
+        public static void RemoveFromCircle(List<byte> circle, int currentPos = 0)
+        {
+            bool isRemovable = false;
+            while (circle.Count > 1)
+            {
+                if (isRemovable)
+                {
+                    circle.RemoveAt(currentPos);
+                    isRemovable = false;
+                }
+                else
+                {
+                    currentPos++;
+                    isRemovable = true;
+                }
+            }
+            
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            #region Execution
+            int n = 0;// base initialization for sure to be a good code :P
+            Console.Write("Enter the number  of people in the cirlce of Lost=");
+            if (int.TryParse(Console.ReadLine(),out n))
+            {
+                //Creating of collection 
+                List<byte> circle = new List<byte>();
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    circle.Add((byte)rnd.Next(0,7)); // we do the random bytes from 0-7. Doesn't metter the names of a ma to survive
+                }
+                RemoveFromCircle(circle, n);
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Input Error!");
+            }
+            
+            #endregion
+        }
+    }
+}
---
+++ XtEpamTask03_2/XtEpamTask03_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XtEpamTask03_2 // 
+{
+    class Program
+    {
+        /// <summary>
+        /// Frequency of word
+        /// </summary>
+        /// <param name="text">Text</param>
+
+        static Dictionary<string, float> WordFrequencyCollector(string text)
+        {
+            Dictionary<string, float> output = new Dictionary<string, float>();
+
+            List<string> words = text.Split(new char[] { ' ', '.' }).ToList(); //we covert With splitting into array but further have to convert it to List, so it would be better practice to make it work, in sacrfice for little bit speed of code
+            HashSet<string> uniq_words = new HashSet<string>(); //to cut off the doubled words in a line
+            foreach (string word in words)
+            {
+                uniq_words.Add(word.ToLower());
+            }
+            int countWords = words.Count;
+
+            foreach (string word in uniq_words)
+            {
+                float frequencyWord = (float)words.FindAll(t => t.ToLower() == word).Count / countWords;
+                output.Add(word, frequencyWord);
+            }
+
+            return output;
+        
+    }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the text: ");
+            string text = Console.ReadLine();
+
+            Dictionary<string, float> frequencyWordKeeper = WordFrequencyCollector(text);
+
+            foreach (var item in frequencyWordKeeper)
+            {
+                Console.WriteLine("Frequency word {0}: {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
